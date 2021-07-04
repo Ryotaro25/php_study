3550294d370f8854cb6f6e98d3c18a1ec4cb8cec
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2021-07-04 17:47:06 +0900

    create php study on docker

---
+++ www/html/olympic.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tokyo olympic</title>
+</head>
+<?php
+$year1 = "1964";
+$year2 = "2020";
+?>
+<body>
+  <div class="main-contents">
+    前回のオリンピックは、
+    <h1>
+      <?php echo $year1; ?>
+    </h1>
+    <br>
+    次のオリンピックは
+    <h1>
+      <?php echo $year2; ?>
+    </h1>
+  </div>    
+</body>
+</html>
